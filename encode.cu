411f282c7a849254d7c937b761e7436f794a5515
tcronis <46980360+tcronis@users.noreply.github.com>
2019-02-17 13:11:34 -0600

    Changed the methods to deal with L-Mers and altered the flipping of bases to operate on its own method

--- encode.cu
+++ encode.cu
@@ -2,63 +2,70 @@
 #include <stdint.h>
 #include <inttypes.h>
 #include <bitset>
-__host__ __device__ void encodData(char *val, int KSize){
-    //k defines the number of bits revelant to the bit mask
-    // k must be strictly less than 32 for now, since a long long is 64 bits 
+__host__ __device__ void baseToLong(char *val, int KSize, long long unsigned *result){
     /*
         A = 00;
         C = 01;
         G = 10;
         T = 11;
-    
     */
     long long unsigned oneflip = 1;
-    long long unsigned result = 0;
-    if (KSize <= 32)
-    {
-        for (int i = 0; i < KSize; i++) {
-            int aciiVal = int(val[i]);
-            //A val
-            if (aciiVal == 65 || aciiVal == 97) {
-                //shift left twice
-                result = result << 1;
-                result = result << 1;
-            }
-            //C val
-            else if (aciiVal == 67 || aciiVal == 99) {
-                //shift left twice
-                result = result << 1;
-                result = result << 1;
-                //flip last bit
-                result = result | oneflip;
+    // long long unsigned result = 0;
 
-            }
-            //G val
-            else if (aciiVal == 71 || aciiVal == 103) {
-                //shift left once
-                result = result << 1;
-                //flip first 1
-                result = result | 1;
-                //shit left again
-                result = result << 1;
-            }
-            //T val
-            else if (aciiVal == 84 || aciiVal == 116) {
-                //shift left once
-                result = result << 1;
-                //flip first 1
-                result = result | 1;
-                //shift left once
-                result = result << 1;
-                //flip first 1
-                result = result | 1;
-            }
-            //default bad val
-            else{
-                printf("Bad Input Flag Triggered \n");
-                break;
-            }
+    for (int i = 0; i < KSize; i++) {
+        int aciiVal = int(val[i]);
+        //A val
+        if (aciiVal == 65 || aciiVal == 97) {
+            //shift left twice
+            (*result) = (*result) << 1;
+            (*result) = (*result) << 1;
+        }
+        //C val
+        else if (aciiVal == 67 || aciiVal == 99) {
+            //shift left twice
+            (*result) = (*result) << 1;
+            (*result) = (*result) << 1;
+            //flip last bit
+            (*result) = (*result) | oneflip;
+
+        }
+        //G val
+        else if (aciiVal == 71 || aciiVal == 103) {
+            //shift left once
+            (*result) = (*result) << 1;
+            //flip first 1
+            (*result) = (*result) | 1;
+            //shit left again
+            (*result) = (*result) << 1;
+        }
+        //T val
+        else if (aciiVal == 84 || aciiVal == 116) {
+            //shift left once
+            (*result) = (*result) << 1;
+            //flip first 1
+            (*result) = (*result) | 1;
+            //shift left once
+            (*result) = (*result) << 1;
+            //flip first 1
+            (*result) = (*result) | 1;
+        }
+        //default bad val
+        else{
+            printf("Bad Input Flag Triggered \n");
+            break;
         }
     }
+
+
+}
+
+__host__ __device__ void encodData(char *val, int KSize){ 
+    long long unsigned kMer = 0;        //create the var that will be the K-Mer
+	baseToLong(val, KSize, (&kMer));       //transform the bases into the long long representation
+
+    long long unsigned lMer = 0;         //create the var that will be the L-Mer
+    int LSize = KSize - 1;              //reduce the size sine L = K - 1
+    baseToLong(val, LSize, (&lMer));       //transform the bases into the long long representation
+    
     //next step is to put the encoded K-mer into the hash table
 }
