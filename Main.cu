e6bce1936de09fa1f3d9f3ec5ee37c805dd7c82e
tcronis <46980360+tcronis@users.noreply.github.com>
2019-03-10 03:05:21 -0500

    Added some comments for future changes for the Main

--- Main.cu
+++ Main.cu
@@ -2,6 +2,7 @@
 
 // const int maxKSize = 105;
 int main(int argc, char * argv[]){
+	//will need to create some input file method that will take the input from the user (KSize & fileName)
     read_fasta(30, "test.fasta");
 
 	return 0;
