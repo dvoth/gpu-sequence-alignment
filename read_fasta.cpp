73c24483430cb02193ca3502770529e379c569a2
tcronis <46980360+tcronis@users.noreply.github.com>
2019-03-25 00:06:38 -0500

    Added the changes for it to take the linked lists to pass to encoding

--- read_fasta.cpp
+++ read_fasta.cpp
@@ -1,10 +1,13 @@
+#include "Encode.cu"
+#include "mer_list.cuh"
+
 #include <iostream>
 #include <fstream>
 #include <string>
 #include <stdio.h>
 #include <string.h>
 
-void read_fasta(int k, std::string file_name){
+void read_fasta(int k, std::string file_name, dataList *kmerHead, dataList *lmerHead){
     std::ifstream in_file(file_name);
     int index_start = 0;
     int index_length = k;
@@ -17,6 +20,7 @@ void read_fasta(int k, std::string file_name){
         // Check to make sure we are not on a header line, as we ignore those.
         if(line_in.substr(0, 1) != ">"){
             while(!line_finished){
+                
                 // This checks to see that the end of the k-mer is still a char (one of the bases).
                 // If it is not, we cannot continue to read k-mers, as we have reached the end of all possibilities.
                 if(line_in[k + index_start - 1] != '\0' && isalpha(line_in.at(k + index_start - 1))){
@@ -27,14 +31,16 @@ void read_fasta(int k, std::string file_name){
                     index_start++;
                     
                     // Pass the k-mer to the encoder for work.
-                    std::cout << k_mer_found << " - " << line_in_substr.length() << std::endl;
+                    // std::cout << k_mer_found << " - " << line_in_substr.length() << std::endl;
+                    
+                    encodeData(k_mer_found, k, kmerHead, lmerHead);
                 }
                 else{
                     line_finished = true;
                 }
             }
         }
-        std::cout << std::endl;
+        // std::cout << std::endl;
         index_start = 0;
         line_finished = false;
     }
